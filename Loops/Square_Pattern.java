6c7849a5e4fb33409c7f3ca5bb6d824c5bb1085c
Mukesh kumar <98644411+Mukeshkumar1611@users.noreply.github.com>
2022-05-29 15:21:33 +0530

    Program to print Square Pattern

---
+++ Loops/Square_Pattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Square_pattern {
+
+
+	public static void main(String[] args) {
+		
+		Scanner mk = new Scanner(System.in);
+		System.out.print("Enter Number: ");
+        int n = mk.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+		
+	}
+
+}
